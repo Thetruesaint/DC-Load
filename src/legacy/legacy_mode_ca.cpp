0208500302c9dd8acea8aea6907da9c9420125c5
Thetruesaint <thetruesaint@hotmail.com>
2026-03-08 20:48:09 -0300

    Refactor calibration mode to consume app contexts

--- src/legacy/legacy_mode_ca.cpp
+++ src/legacy/legacy_mode_ca.cpp
@@ -3,7 +3,11 @@
 #include "../variables.h"
 #include "../ui_lcd.h"
 #include "../funciones.h"
+#include "../app/app_load_context.h"
+#include "../app/app_runtime_context.h"
+#include "../app/app_setpoint_context.h"
 #include "../app/app_value_input.h"
+#include "../app/app_value_result_context.h"
 
 void legacy_calibration_mode() {
   if (!modeConfigured) {
@@ -12,11 +16,12 @@ void legacy_calibration_mode() {
   }
 
   if (!modeInitialized) {
-    if (x == 1) {
+    const float selection = app_value_result_get();
+    if (selection == 1.0f) {
       calibrateVoltage = true;
       Sns_Volt_Calib_Fact = 1.0;
       Sns_Volt_Calib_Offs = 0.0;
-    } else if (x == 2) {
+    } else if (selection == 2.0f) {
       calibrateVoltage = false;
       Sns_Curr_Calib_Fact = 1.0;
       Sns_Curr_Calib_Offs = 0.0;
@@ -35,15 +40,16 @@ void legacy_calibration_mode() {
     modeInitialized = true;
   }
 
-  reading = encoderPosition / 1000;
-  reading = min(maxReading, max(0.0f, reading));
-  encoderPosition = reading * 1000.0;
+  float readingValue = app_runtime_encoder_position() / 1000.0f;
+  readingValue = min(app_setpoint_max_reading(), max(0.0f, readingValue));
+  app_setpoint_set_reading(readingValue);
+  app_runtime_set_encoder_position(readingValue * 1000.0f);
   Cursor_Position();
 
-  if (!toggle) {
+  if (!app_load_is_enabled()) {
     return;
   }
-  setCurrent = reading * 1000;
+  app_load_set_set_current_mA(readingValue * 1000.0f);
 }
 
 void legacy_calibration_setup() {
@@ -52,6 +58,7 @@ void legacy_calibration_setup() {
   printLCD(0, 1, F("1-Voltage 2-Current"));
   printLCD(0, 2, F("3-Load    4-Save"));
 
+  float selection = 0.0f;
   do {
     z = 1;
     r = 3;
@@ -60,16 +67,17 @@ void legacy_calibration_setup() {
     if (!Value_Input(z, r, 1, false)) {
       return;
     }
-  } while (x < 1 || x > 4);
+    selection = app_value_result_get();
+  } while (selection < 1.0f || selection > 4.0f);
 
   modeConfigured = true;
-  if (x == 3) {
+  if (selection == 3.0f) {
     Load_Calibration();
     printLCD(12, 3, F("Loaded!"));
     delay(1500);
     modeConfigured = false;
   }
-  if (x == 4) {
+  if (selection == 4.0f) {
     Save_Calibration();
     printLCD(12, 3, F("Saved!"));
     delay(1500);
@@ -91,7 +99,7 @@ void legacy_calibrate(float realValue) {
   if (!firstPointTaken) {
     measuredValue1 = measuredValue;
     realValue1 = realValue;
-    setCurrent1 = setCurrent / 1000;
+    setCurrent1 = app_load_set_current_mA() / 1000.0f;
     firstPointTaken = true;
     modeInitialized = false;
     return;
@@ -99,7 +107,7 @@ void legacy_calibrate(float realValue) {
 
   measuredValue2 = measuredValue;
   realValue2 = realValue;
-  setCurrent2 = setCurrent / 1000;
+  setCurrent2 = app_load_set_current_mA() / 1000.0f;
   Load_OFF();
   firstPointTaken = false;
 
