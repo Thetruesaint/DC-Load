0aa34cfea2ae4aca076d3530d9b49ceeaf5072e3
Thetruesaint <thetruesaint@hotmail.com>
2026-03-10 18:15:24 -0300

    Stop routing CC CP CR through legacy mode dispatch

--- src/legacy/legacy_mode_dispatch.cpp
+++ src/legacy/legacy_mode_dispatch.cpp
@@ -4,16 +4,14 @@
 #include "../config/system_constants.h"
 #include "legacy_mode_bc.h"
 #include "legacy_mode_ca.h"
-#include "legacy_mode_cc.h"
-#include "legacy_mode_cp.h"
-#include "legacy_mode_cr.h"
 #include "legacy_mode_transient.h"
 
 void legacy_run_mode_logic() {
   switch (app_mode_state_mode()) {
-    case CC: legacy_const_current_mode(); break;
-    case CP: legacy_const_power_mode(); break;
-    case CR: legacy_const_resistance_mode(); break;
+    case CC:
+    case CP:
+    case CR:
+      break;
     case BC: legacy_battery_mode(); break;
     case TC: legacy_transient_cont_mode(); break;
     case TL: legacy_transient_list_mode(); break;
